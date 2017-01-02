71f9458b87c151ab2d85453c8b2168576298e876
phix <hickstricks@gmail.com>
2017-01-02 15:54:23 -0800

    adding in semi-colons through the linting mechanism

--- src/routes/GitHub/index.js
+++ src/routes/GitHub/index.js
@@ -1,7 +1,7 @@
 import { injectReducer } from '../../store/reducers';
 
 export default (store) => ({
-  path : 'counter',
+  path : 'github',
   /*  Async getComponent is only invoked when route matches   */
   getComponent (nextState, cb) {
     /*  Webpack - use 'require.ensure' to create a split point
@@ -9,7 +9,7 @@ export default (store) => ({
     require.ensure([], (require) => {
       /*  Webpack - use require callback to define
           dependencies for bundling   */
-      const Counter = require('./containers/CounterContainer').default;
+      const Counter = require('./containers/GithubContainer').default;
       const reducer = require('./modules/counter').default;
 
       /*  Add the reducer to the store on key 'counter'  */
